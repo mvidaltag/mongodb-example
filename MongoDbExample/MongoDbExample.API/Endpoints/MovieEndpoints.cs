9c780e9b724c0400b7825e02aef0d1aec4b07475
Matheus Oliveira Vidal <mvidal@taginfraestrutura.com.br>
2025-07-30 18:23:49 -0300

    Finishing solution.

--- MongoDbExample/MongoDbExample.API/Endpoints/MovieEndpoints.cs
+++ MongoDbExample/MongoDbExample.API/Endpoints/MovieEndpoints.cs
@@ -8,14 +8,16 @@ public static class MovieEndpoints
 {
     public static void MapMovieEndpoints(this WebApplication app)
     {
-        app.MapGet("/", () => "hello world!");
-
         app.MapGet("/movies", async (IMovieRepository repo) =>
         {
             var movies = await repo.GetAllAsync();
             var output = movies.Select(CustomMapper.ToOutputDto);
             return Results.Ok(output);
-        });
+        })
+        .WithName("GetAllMovies")
+        .WithTags("Movies")
+        .WithSummary("Lists all movies")
+        .Produces<IEnumerable<MovieOutputDto>>(StatusCodes.Status200OK);
 
         app.MapGet("/movies/{id}", async (string id, IMovieRepository repo) =>
         {
@@ -23,7 +25,12 @@ public static class MovieEndpoints
             if (movie is null) return Results.NotFound();
             var output = CustomMapper.ToOutputDto(movie);
             return Results.Ok(output);
-        });
+        })
+        .WithName("GetMovieById")
+        .WithTags("Movies")
+        .WithSummary("Gets a movie by its ID")
+        .Produces<MovieOutputDto>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound);
 
         app.MapPost("/movies", async (MovieInputDto input, IMovieRepository repo) =>
         {
@@ -31,19 +38,31 @@ public static class MovieEndpoints
             await repo.AddAsync(movie);
             var output = CustomMapper.ToOutputDto(movie);
             return Results.Created($"/movies/{movie.Id}", output);
-        });
+        })
+        .WithName("CreateMovie")
+        .WithTags("Movies")
+        .WithSummary("Creates a new movie")
+        .Produces<MovieOutputDto>(StatusCodes.Status201Created);
 
         app.MapPut("/movies/", async (MovieDto input, IMovieRepository repo) =>
         {
             var movie = CustomMapper.ToMovie(input);
             await repo.UpdateAsync(movie);
             return Results.NoContent();
-        });
+        })
+        .WithName("UpdateMovie")
+        .WithTags("Movies")
+        .WithSummary("Updates an existing movie")
+        .Produces(StatusCodes.Status204NoContent);
 
         app.MapDelete("/movies/{id}", async (string id, IMovieRepository repo) =>
         {
             await repo.DeleteAsync(id);
             return Results.NoContent();
-        });
+        })
+        .WithName("DeleteMovie")
+        .WithTags("Movies")
+        .WithSummary("Deletes a movie by its ID")
+        .Produces(StatusCodes.Status204NoContent);
     }
 }
